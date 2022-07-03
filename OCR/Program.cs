b40dceb17ee2b6456c8cc0faf0854ec62fbaabce
Wesley Gomes Queiroz <wgomesq@santillana.local>
2022-07-03 16:00:22 -0300

    Primeiro Commit

---
+++ OCR/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IronOcr;
+
+namespace OCR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new IronTesseract().Read(@"images\teste.jpg").Text);
+        }
+    }
+}
